f61b67250a127e60746776b9e17339a01c4ea037
kamtou boris <boriskamtou@gmail.com>
2020-05-25 16:18:44 +0200

    add page_indicator

--- lib/src/screens/onboarding_screen.dart
+++ lib/src/screens/onboarding_screen.dart
@@ -9,6 +9,43 @@ class OnboardingScreen extends StatefulWidget {
 }
 
 class _OnboardingScreenState extends State<OnboardingScreen> {
+  final _pageController = PageController(initialPage: 0);
+
+ final int _numPage = 4;
+  int _currentPage = 0;
+
+
+  List<Widget> _buildPageIndicator() {
+    List<Widget> list = [];
+
+    for (int i = 0; i < _numPage; i++) {
+      list.add(i == _currentPage ? _indicator(true) : _indicator(false));
+    }
+
+    return list;
+  }
+
+  Widget _indicator(bool isActive) {
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
+      margin: EdgeInsets.symmetric(horizontal: 4),
+      height: isActive ? 16 : 8,
+      width: isActive ? 16 : 8,
+      decoration: isActive
+          ? BoxDecoration(
+              border: Border.all(
+                width: 2.5,
+                color: Color(0xFF7777FF),
+              ),
+              shape: BoxShape.circle,
+            )
+          : BoxDecoration(
+              shape: BoxShape.circle,
+              color: Color(0xFF979797),
+            ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
@@ -17,10 +54,20 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
       body: Container(
         height: screenHeight,
         width: screenWidth,
+        padding: EdgeInsets.only(
+          left: 16,
+          right: 16,
+          bottom: 16,
+        ),
         child: Column(
           children: <Widget>[
             Expanded(
               child: PageView(
+                onPageChanged: (page) {
+                  setState(() {
+                    _currentPage = page;
+                  });
+                },
                 children: <Widget>[
                   OnboardingModel(
                     imagePath: 'assets/images/img1.png',
@@ -53,6 +100,35 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                   ),
                 ],
               ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                FlatButton(
+                  onPressed: () {},
+                  child: Text(
+                    'Skip',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color(0xFF707070),
+                    ),
+                  ),
+                ),
+                Row(
+                  children: _buildPageIndicator(),
+                ),
+                RaisedButton(
+                  onPressed: () {},
+                  elevation: 0.75,
+                  child: Text(
+                    'Next',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                    ),
+                  ),
+                )
+              ],
             ),
           ],
         ),
--- lib/src/theme/theme.dart
+++ lib/src/theme/theme.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
 
 final myTheme = ThemeData(
-  primaryColor: Colors.blueGrey.shade300,
+  primaryColor: Colors.white,
   scaffoldBackgroundColor: Color(0xFFF8F8FF),
   fontFamily: 'Sen',
   textTheme: TextTheme(
     headline1: TextStyle(
-    color: Color(0xFF7777FF),
+      color: Color(0xFF7777FF),
     ),
     headline2: TextStyle(
       color: Colors.black,
     ),
     subtitle1: TextStyle(
-      color: Color(0xFF707070), 
-    )
+      color: Color(0xFF707070),
+    ),
   ),
-  
   buttonTheme: ButtonThemeData(
     buttonColor: Color(0xFF7777FF),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(30),
+    ),
     minWidth: 100,
-    height: 60,
+    height: 40,
   ),
   visualDensity: VisualDensity.adaptivePlatformDensity,
 );
